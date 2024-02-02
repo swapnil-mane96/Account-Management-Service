d9381339b2e92ea6ceb2e9d0b0c31e9c29cfd1c7
Swapnil Mane <maneswap1@gmail.com>
2024-02-02 21:39:10 +0530

    added account-management-service

---
+++ AccountManagement-Service/src/main/java/com/photoapp/account/controller/AccountController.java
@@ -0,0 +1,15 @@
+package com.photoapp.account.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+	
+	@GetMapping("/status/check")
+	public String getStatus() {
+		return "Account service is working fine...";
+	}
+}
---
+++ AccountManagement-Service/src/test/java/com/photoapp/account/AccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
